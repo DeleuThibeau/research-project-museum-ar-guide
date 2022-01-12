28478211a1874eb6c09a81db8bddc0cb4da79d89
Thibeau <thibeau.deleu@student.howest.be>
2022-01-12 18:03:48 +0100

    Added dynamic text implementation with Json in a-text

---
+++ Code/app.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    dynamicText();
+});
+
+const dynamicText = function(){
+    let json = "";
+    fetch("./assets/json/descriptions.json")
+    .then(response => {
+    return response.json();
+    })
+    .then((jsondata) => {
+        json = jsondata
+        if(json){
+            console.log(json)
+            AFRAME.registerComponent('render-dynamic-text', {
+                init: function () {
+                  var sceneE1 = this.el;
+                  var atext = sceneE1.querySelector("a-text")
+                  var aEntity = sceneE1.querySelector('a-entity')
+                  console.log(sceneE1)
+                  console.log(aEntity)
+                  console.log(atext)
+                  atext.setAttribute('value', json.painting02.description)
+                }
+              });
+        }
+    })
+
+
+}
