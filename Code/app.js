a3d9363ec41f524c67fbba69cd9db79e50583f22
Thibeau <thibeau.deleu@student.howest.be>
2022-01-14 18:50:14 +0100

    Added new model and made all pages accesible trough the index page, plus startup for dynamic model rendering

--- Code/app.js
+++ Code/app.js
@@ -1,9 +1,26 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    dynamicText();
+document.addEventListener('DOMContentLoaded', function(){
+  console.log('DOM fully loaded and parsed');
+  renderCorrectFunctions();
 });
 
+
+const renderCorrectFunctions = function(){
+  var imagePage = document.querySelector('.js-scene-image')
+  var imageCode = document.querySelector('.js-scene-code')
+  console.log(imagePage)
+  console.log(imageCode)
+
+    if(imagePage){
+      // renderCorrectModelOnImagePage();
+       console.log("run function here")
+    }else if(imageCode){
+      dynamicText();
+    }
+
+}
+
 const dynamicText = function(){
+    console.log("CODEPAGEFUNCTION")
     let json = "";
     fetch("./assets/json/descriptions.json")
     .then(response => {
@@ -21,6 +38,7 @@ const dynamicText = function(){
                   console.log(sceneE1)
                   console.log(aEntity)
                   console.log(atext)
+                  console.log(atext.setAttribute('value', json.painting02.description))
                   atext.setAttribute('value', json.painting02.description)
                 }
               });
@@ -30,7 +48,39 @@ const dynamicText = function(){
 
 }
 
-const cameraSelection = function(){
-    var secondCameraEl = document.querySelector('#second-camera');
-    secondCameraEl.setAttribute('camera', 'active', true);
-}
+// TensorFlow JS might be interesting for if conditioning and setting the correct path.
+// const renderCorrectModelOnImagePage = function(){
+//     console.log("IMAGEPAGEFUNCTION")
+//     AFRAME.registerComponent('render-correct-model', {
+//         init: function () {
+//           var sceneE1 = this.el;
+//           var aEntity = sceneE1.querySelector('a-entity')
+//           var anft = sceneE1.querySelector('a-nft')
+//           var aAssets = sceneE1.querySelector('a-assets')
+//           var aAssetsItem = aAssets.querySelector('a-asset-item')
+//           // console.log(sceneE1)
+//           // console.log(aEntity)
+//           console.log(anft)
+//           // console.log(aAssets)
+//           // console.log(aAssetsItem)
+//           var nftVariableArray = anft.getAttribute('url').split('/')
+//           var nftVariable = nftVariableArray[2]
+//           console.log(nftVariable)
+
+//           if(nftVariable == "image01"){
+//             console.log(nftVariable);
+//             console.log(aAssetsItem.getAttribute('src'));
+//             aAssetsItem.setAttribute('src', 'assets/models/painting02/scene.gltf');
+//             console.log(aAssetsItem.getAttribute('src'));
+
+//           } else if (nftVariable == "image02"){
+//             console.log(nftVariable);
+//             console.log(aAssetsItem.getAttribute('src'));
+//             aAssetsItem.setAttribute('src', 'assets/models/painting02/scene.gltf');
+//             console.log(aAssetsItem.getAttribute('src'));
+//           }
+//         }
+//       });
+// }
+
+
